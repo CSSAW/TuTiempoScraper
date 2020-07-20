0923ce300235fb43acbf2c2df2c83e1b2e14248e
Andrew Ruder <49738576+rudera@users.noreply.github.com>
2020-07-19 23:55:31 -0700

    Remove columns for wind data

--- removenull.py
+++ removenull.py
@@ -1,14 +1,14 @@
 '''
 Author: Andrew Ruder
-Last edited: 07/12/2020
+Last edited: 07/19/2020
 
 Takes original scraped data and creates a new csv where it only
 writes inputs from days where temperature was recorded
 '''
 import csv
 with open('tutiempodataset.csv', 'r') as inp:
-	filename = "nonull.csv"
+	filename = "relevantWeatherData.csv"
 	f = open(filename, "w")
 	for row in csv.reader(inp):
 		if row[4] != '-':
-			f.write(row[0] + "," +row[1] + "," + row[2] + "," + row[3]  + "," + row[4] + "," +row[5] + "," +row[6] + "," +row[7] + "," +row[8] + "," + row[9] + "," +row[10] + "\n")
+			f.write(row[0] + "," +row[1] + "," + row[2] + "," + row[3]  + "," + row[4] + "," +row[5] + "," +row[6] + "," +row[7] + "," +row[8] + "\n")
